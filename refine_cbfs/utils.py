d53a3510727b9eca87f26d3630ffadce2bfcaabd
Sander Tonkens <stonkens@ucsd.edu>
2023-12-13 13:47:18 -0800

    bug fix

--- refine_cbfs/utils.py
+++ refine_cbfs/utils.py
@@ -1,6 +1,7 @@
 import functools
 import numpy as np
 import jax.numpy as jnp
+from jax import lax
 from hj_reachability import Grid
 
 
@@ -76,3 +77,115 @@ def hj_solve(solver_settings, dyn_hjr, grid, times, init_values):
     
     return jnp.min(jnp.array(values), axis=0)
         
+    
+
+
+# def animate_figure(ax, static_objects, dynamic_objects):
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111)
+
+
+#     ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#             tabular_cbf.vf_table[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[0], colors='grey', linewidths=4)  
+#     cont3 = ax.contourf(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#             sdf_values[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[-10, 0], colors='red')
+#     cont2 = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#             sdf_values[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[0], colors='k', linewidths=4)
+#     cont = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#             target_values_full[0][:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[0], colors='green', linewidths=2)
+#     ax.set_xlabel('$y$ (Horizontal)')
+#     ax.set_ylabel('$z$ (Vertical)')
+#     tx = ax.set_title(f'$v_y=0, v_z=0$, HJR time $t=0$')
+
+# # Update function to draw contours for a given idi value
+# def update(idi):
+#     global cont, cont2, cont3
+#     arr = target_values_full[idi][:, :, grid.shape[2] // 2, grid.shape[3] // 2].T
+#     arr_sdf = sdf_full[idi][:, :, grid.shape[2] // 2, grid.shape[3] // 2].T
+#     vmax = np.abs(arr).max()
+#     # ax.clear()
+#     cont.collections[0].remove()
+#     cont = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#                arr, levels=[0], colors='green')
+#     cont2.collections[0].remove()
+#     cont2 = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#                           arr_sdf, levels=[0], colors='k', linewidths=4)
+#     cont3.collections[0].remove()
+#     cont3 = ax.contourf(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#            arr_sdf, levels=[-10, 0], colors='red')
+#     tx.set_text('$v_y=0, v_z=0$, HJR time t={:.2f}'.format(np.abs(times[idi].item())))
+
+
+# # Animate with idi values from 0 to 11
+# ani = FuncAnimation(fig, update, frames=range(len(times)));
+# plt.close()
+# HTML(ani.to_jshtml())
+
+
+
+# #################################
+# def find_closest_time(df, time_ind):
+#     return df.t.iloc[df.t.sub(time_ind).abs().idxmin()]
+
+
+# def visualize(fig, timestamps, contours = None, dataframe = None, **kwargs):
+    
+
+
+# ss_exp = StateSpaceExperiment('quad', x_indices=[0, 1], start_x=x0)
+# # Set up the figure and axis
+# fig = plt.figure(figsize=(20,10))
+# ax = fig.add_subplot(111)
+# alphas = [0.1, 0.5, 0.5, 1.0]
+# nbr_controllers = len(results_df.controller.unique())
+
+# plt.legend(results_df.controller.unique())
+# ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#            tabular_cbf.vf_table[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[0], colors='grey', linewidths=4)  
+# cont3 = ax.contourf(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#            sdf_values[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[-10, 0], colors='red')
+# cont2 = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#            sdf_values[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[0], colors='k', linewidths=4)
+# cont = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#            target_values_f(0.0)[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T, levels=[0], colors='green', linewidths=2)
+# ax.set_xlabel('$y$ (Horizontal)', fontsize=20)
+# ax.set_ylabel('$z$ (Vertical)', fontsize=20)
+# tx = ax.set_title('$v_y=0, v_z=0$, HJR time $t=0$')
+# ss_exp.plot(batched_dyn, results_df, ax=ax, add_direction=False, max_time=0.0, alpha=alphas)
+# ax.legend(ax.lines[::len(ax.lines) // nbr_controllers], results_df.controller.unique(), loc="upper center", ncol=4, fontsize=20)
+
+# # Update function to draw contours for a given idi value
+# def update(time):
+#     global cont, cont2, cont3
+#     arr = target_values_f(time)[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T
+#     sdf_arr = sdf_values_f(time)[:, :, grid.shape[2] // 2, grid.shape[3] // 2].T
+#     # ax.clear()
+#     for line in ax.lines:
+#         line.remove()
+#     for patch in ax.patches:
+#         patch.remove()
+#     ax.set_prop_cycle(None)
+#     cont.collections[0].remove()
+#     cont = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#                arr, levels=[0], colors='green')
+#     cont2.collections[0].remove()
+#     cont2 = ax.contour(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#                             sdf_arr, levels=[0], colors='k', linewidths=4)
+#     cont3.collections[0].remove()
+#     cont3 = ax.contourf(grid.coordinate_vectors[0], grid.coordinate_vectors[1],
+#                         sdf_arr, levels=[-10, 0], colors='red')
+#     ss_exp.plot(batched_dyn, results_df, ax=ax, add_direction=False, max_time=time, alpha=alphas)
+#     closest_time = find_closest_time(results_df, time)
+#     curr_vals = results_df[(results_df.t == closest_time) & (results_df.measurement.isin(["y", "z", "tan(phi)"]))].value.values.reshape(nbr_controllers, -1)
+#     colors = []
+#     for line in ax.lines[::len(ax.lines) // nbr_controllers]:
+#         colors.append(line.get_color())
+#     for i, curr_val in enumerate(curr_vals):
+#         # get color from prop_cycle 
+#         get_drone(ax, curr_val[0], curr_val[1], np.arctan(-curr_val[2]), rel_size=0.3, height_scale=0.9, alpha=alphas[i], color=colors[i])
+#     tx.set_text('$v_y=0, v_z=0$, Simulation time t={:.2f}'.format(np.abs(time)))
+#     fig.tight_layout()
+
+# ani = FuncAnimation(fig, update, frames=np.linspace(0,20,100))
+# plt.close()
+# HTML(ani.to_jshtml())
